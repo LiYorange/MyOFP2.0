aa3c12bb613e4b705d2a0907ab4a7fe12e39cef4
LiYorange <cc11lyr@163.com>
2020-12-17 14:35:11 +0800

    第一次更新

---
+++ bin/launch.py
@@ -0,0 +1,27 @@
+# -------------------------------------------------------------------------------
+# Name:         launch
+# Description:
+# Author:       A07567
+# Date:         2020/12/16
+# Description:  
+# -------------------------------------------------------------------------------
+import logging
+from PySide2.QtWidgets import QWidget, QApplication
+from PySide2.QtUiTools import QUiLoader
+
+
+class DrawWindow(QWidget):
+    def __init__(self):
+        super(DrawWindow, self).__init__()
+        self.window = QUiLoader().load('../res/ui/draw.ui')
+        self.window.plot_pushButton.clicked.connect(self.plot)
+
+    def plot(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    draw_window = DrawWindow()
+    draw_window.window.show()
+    app.exec_()
